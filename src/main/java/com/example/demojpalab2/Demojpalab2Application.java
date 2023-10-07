6daff9b37e7b565a9fba6ad13dd90e62138a316e
Walter Leturia <leturia@live.com>
2023-10-07 11:05:08 -0500

    First commit

---
+++ src/main/java/com/example/demojpalab2/Demojpalab2Application.java
@@ -0,0 +1,13 @@
+package com.example.demojpalab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demojpalab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demojpalab2Application.class, args);
+	}
+
+}
